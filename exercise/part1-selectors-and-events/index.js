26383969cbb3676fcc557c5c1818ed3b6b25f783
Zhen3r <xzzxzz2010@163.com>
2022-02-07 16:43:47 -0500

    finish

--- exercise/part1-selectors-and-events/index.js
+++ exercise/part1-selectors-and-events/index.js
@@ -14,30 +14,32 @@ Part 1: Set the variable below equal to the paragraph element representing the
 first test result.
 ==================== */
 
-let firstResult;
+let firstResult = document.getElementById('result-1');
 
 /* ====================
 Parts 2: Set the variable below equal to a collection of the paragraph
 elements representing the 2nd and 3rd results.
 ==================== */
 
-let secondAndThirdResults;
+let secondAndThirdResults = document.getElementsByClassName('result result-2-3');
 
 /* ====================
 Parts 3: Set the variable below equal to a collection of the paragraph
 elements representing the all of the results.
 ==================== */
 
-let allResults;
+let allResults = document.getElementsByTagName('tr');
 
 /* ====================
 Part 4: Add an event listener to the button in problem 4 that changes the
 button's own text to "I'm Clicked!"
 ==================== */
 
-let imClickedButton;
+let imClickedButton = document.getElementById('im-clicked-button');
 if (imClickedButton) {
-  imClickedButton.addEventListener('click', (evt) => {});
+  imClickedButton.addEventListener('click', (evt) => {
+    imClickedButton.innerHTML = "I'm Clicked!";
+  });
 }
 
 /* ====================
@@ -54,9 +56,13 @@ HINT: You may need some global state for this problem.
 let spanContainer = document.querySelector('#span-container');
 spanContainer.appendChild(htmlToElement('<span>0</span>'));
 
-let addSpanButton;
+let count = 0;
+let addSpanButton = document.querySelector('#add-span-button');
 if (addSpanButton) {
-  addSpanButton.addEventListener('click', (evt) => {});
+  addSpanButton.addEventListener('click', (evt) => {
+    count++;
+    spanContainer.appendChild(htmlToElement(`<span>${count}</span>`));
+  });
 }
 
 /* =====================
@@ -77,7 +83,7 @@ function updateResults() {
   // Part 2
   try {
     if (secondAndThirdResults.length !== 2) {
-      console.log(`Failed part 2: Variable should represent exactly 2 elements, not ${secondAndThirdResults.length}.`)
+      console.log(`Failed part 2: Variable should represent exactly 2 elements, not ${secondAndThirdResults.length}.`);
     } else {
       document.querySelector('#result-2').textContent = 'Success!';
     }
@@ -89,7 +95,7 @@ function updateResults() {
   // Part 3
   try {
     if (allResults.length < 3) {
-      console.log(`Failed part 3: Variable should represent ... elements, not ${secondAndThirdResults.length}.`)
+      console.log(`Failed part 3: Variable should represent ... elements, not ${secondAndThirdResults.length}.`);
     } else {
       document.querySelector('#result-3').textContent = 'Success!';
     }
@@ -104,16 +110,16 @@ function updateResults() {
       if (imClickedButton.textContent === 'I\'m Clicked!') {
         document.querySelector('#result-4').textContent = 'Success!';
       }
-    })
+    });
   }
 
   // Part 5
   spanContainer.addEventListener('DOMNodeInserted', (evt) => {
     if (spanContainer.children.length === 6
-        && spanContainer.lastChild.textContent === '5') {
+      && spanContainer.lastChild.textContent === '5') {
       document.querySelector('#result-5').textContent = 'Success!';
     }
-  })
+  });
 }
 
 updateResults();
