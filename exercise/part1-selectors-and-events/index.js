db4027c51d401e1a4bfab961001ec9b9d8309826
Carlos Cai <caiziyuan2009@126.com>
2022-02-08 16:14:58 -0500

    exercise1

--- exercise/part1-selectors-and-events/index.js
+++ exercise/part1-selectors-and-events/index.js
@@ -14,30 +14,32 @@ Part 1: Set the variable below equal to the paragraph element representing the
 first test result.
 ==================== */
 
-let firstResult;
+let firstResult = document.getElementById("result-1");
 
 /* ====================
 Parts 2: Set the variable below equal to a collection of the paragraph
 elements representing the 2nd and 3rd results.
 ==================== */
 
-let secondAndThirdResults;
+let secondAndThirdResults = [document.getElementById("result-2"), document.getElementById("result-3")];
 
 /* ====================
 Parts 3: Set the variable below equal to a collection of the paragraph
 elements representing the all of the results.
 ==================== */
 
-let allResults;
+let allResults = document.getElementsByTagName("p");
 
 /* ====================
 Part 4: Add an event listener to the button in problem 4 that changes the
 button's own text to "I'm Clicked!"
 ==================== */
 
-let imClickedButton;
+let imClickedButton = document.getElementById("im-clicked-button");
 if (imClickedButton) {
-  imClickedButton.addEventListener('click', (evt) => {});
+  imClickedButton.addEventListener('click', (evt) => {
+    document.getElementById("im-clicked-button").innerHTML = "I'm Clicked!";
+  });
 }
 
 /* ====================
@@ -54,9 +56,13 @@ HINT: You may need some global state for this problem.
 let spanContainer = document.querySelector('#span-container');
 spanContainer.appendChild(htmlToElement('<span>0</span>'));
 
-let addSpanButton;
+let span_number = 0;
+let addSpanButton = document.getElementById("add-span-button");
 if (addSpanButton) {
-  addSpanButton.addEventListener('click', (evt) => {});
+  addSpanButton.addEventListener('click', (evt) => {
+    span_number++;
+    spanContainer.appendChild(htmlToElement(`<span>${span_number}</span>`));
+  });
 }
 
 /* =====================
