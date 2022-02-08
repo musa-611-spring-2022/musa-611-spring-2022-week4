c03028bd616e97209530ab138b1bafa84bd8571d
Mjumbe Poe <mjumbewu@gmail.com>
2022-02-07 23:15:06 -0500

    Fix linter errors

--- exercise/part1-selectors-and-events/tools.js
+++ exercise/part1-selectors-and-events/tools.js
@@ -4,15 +4,17 @@ representing the tags, using the `template` feature of HTML. See the following
 for more information: https://stackoverflow.com/a/35385518/123776
 ==================== */
 
+/* eslint-disable no-unused-vars */
+
 /**
  * @param {String} HTML representing a single element
  * @return {Element}
  */
 function htmlToElement(html) {
-    var template = document.createElement('template');
-    html = html.trim(); // Never return a text node of whitespace as the result
-    template.innerHTML = html;
-    return template.content.firstChild;
+  const template = document.createElement('template');
+  const trimmedHtml = html.trim(); // Never return a text node of whitespace as the result
+  template.innerHTML = trimmedHtml;
+  return template.content.firstChild;
 }
 
 /**
@@ -20,7 +22,7 @@ function htmlToElement(html) {
  * @return {NodeList}
  */
 function htmlToElements(html) {
-    var template = document.createElement('template');
-    template.innerHTML = html;
-    return template.content.childNodes;
+  const template = document.createElement('template');
+  template.innerHTML = html;
+  return template.content.childNodes;
 }
