5355d604f91516538f091c0c6fc7e4f32128b046
Jiali Yao <89944340+Jellyyyyy@users.noreply.github.com>
2022-02-09 20:01:23 -0500

    Update index.js

--- exercise/part1-selectors-and-events/index.js
+++ exercise/part1-selectors-and-events/index.js
@@ -14,31 +14,35 @@ Part 1: Set the variable below equal to the paragraph element representing the
 first test result.
 ==================== */
 
-let firstResult;
+let firstResult = document.getElementById("result-1");
 
 /* ====================
 Parts 2: Set the variable below equal to a collection of the paragraph
 elements representing the 2nd and 3rd results.
 ==================== */
 
-let secondAndThirdResults;
+let secondAndThirdResults = document.getElementsByClassName("result result-2-3");
 
 /* ====================
 Parts 3: Set the variable below equal to a collection of the paragraph
 elements representing the all of the results.
 ==================== */
 
-let allResults;
+let allResults = document.getElementsByTagName("p");
 
 /* ====================
 Part 4: Add an event listener to the button in problem 4 that changes the
 button's own text to "I'm Clicked!"
 ==================== */
 
-let imClickedButton;
+let imClickedButton = document.getElementById("im-clicked-button");
 if (imClickedButton) {
-  imClickedButton.addEventListener('click', (evt) => {});
+  imClickedButton.addEventListener('click', (evt) => {
+    document.getElementById("im-clicked-button")
+    .textContent = "I\'m Clicked!";
+  });
 }
+
 
 /* ====================
 Part 5: Add an event listener to the button in problem 5 that creates a new span
@@ -56,7 +60,9 @@ spanContainer.appendChild(htmlToElement('<span>0</span>'));
 
 let addSpanButton;
 if (addSpanButton) {
-  addSpanButton.addEventListener('click', (evt) => {});
+  addSpanButton.addEventListener('click', (evt) => {
+
+  });
 }
 
 /* =====================
