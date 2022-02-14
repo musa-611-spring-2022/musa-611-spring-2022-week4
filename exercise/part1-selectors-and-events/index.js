6ccb19438828c4bd895c355430b995f3a52a20f4
golete <golete@gmail.com>
2022-02-14 02:23:27 -0500

    First excercise initial commit

--- exercise/part1-selectors-and-events/index.js
+++ exercise/part1-selectors-and-events/index.js
@@ -15,6 +15,7 @@ first test result.
 ==================== */
 
 let firstResult;
+firstResult = document.getElementById("result-1");
 
 /* ====================
 Parts 2: Set the variable below equal to a collection of the paragraph
@@ -22,6 +23,7 @@ elements representing the 2nd and 3rd results.
 ==================== */
 
 let secondAndThirdResults;
+secondAndThirdResults = document.getElementsByClassName("result result-2-3");
 
 /* ====================
 Parts 3: Set the variable below equal to a collection of the paragraph
@@ -29,6 +31,7 @@ elements representing the all of the results.
 ==================== */
 
 let allResults;
+allResults = document.getElementsByTagName('p');
 
 /* ====================
 Part 4: Add an event listener to the button in problem 4 that changes the
@@ -36,9 +39,15 @@ button's own text to "I'm Clicked!"
 ==================== */
 
 let imClickedButton;
+imClickedButton = document.getElementById('im-clicked-button');
 if (imClickedButton) {
-  imClickedButton.addEventListener('click', (evt) => {});
-}
+  imClickedButton.addEventListener('click', (evt) => {
+    document
+      .getElementById('im-clicked-button')
+      .innerHTML = "I'm Clicked!";
+    });
+  }
+
 
 /* ====================
 Part 5: Add an event listener to the button in problem 5 that creates a new span
@@ -52,11 +61,21 @@ HINT: You may need some global state for this problem.
 ==================== */
 
 let spanContainer = document.querySelector('#span-container');
-spanContainer.appendChild(htmlToElement('<span>0</span>'));
+let n = 0
+//let n = 0
+spanContainer.appendChild(htmlToElement('<span>'+n+'</span>'));
 
 let addSpanButton;
+addSpanButton = document.getElementById('add-span-button');
+
 if (addSpanButton) {
-  addSpanButton.addEventListener('click', (evt) => {});
+  addSpanButton.addEventListener('click', (evt) => {
+    n+=1
+    document
+      .getElementById('span-container')
+      .appendChild(htmlToElement('<span>'+n+'</span>'))
+
+  });
 }
 
 /* =====================
