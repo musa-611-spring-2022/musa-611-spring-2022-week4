045e945e8738aaf04c76363241a3f62b9b4048ed
capollon <capollon316@gmail.com>
2022-02-15 13:59:16 -0500

    Exercise pt 1

--- exercise/part1-selectors-and-events/index.js
+++ exercise/part1-selectors-and-events/index.js
@@ -14,21 +14,21 @@ Part 1: Set the variable below equal to the paragraph element representing the
 first test result.
 ==================== */
 
-let firstResult;
+let firstResult = h(document.getElementById("result-1");
 
 /* ====================
 Parts 2: Set the variable below equal to a collection of the paragraph
 elements representing the 2nd and 3rd results.
 ==================== */
 
-let secondAndThirdResults;
+let secondAndThirdResults = document.getElementByClass("result result-2-3");
 
 /* ====================
 Parts 3: Set the variable below equal to a collection of the paragraph
 elements representing the all of the results.
 ==================== */
 
-let allResults;
+let allResults = document.getElementByTagName ("p" )
 
 /* ====================
 Part 4: Add an event listener to the button in problem 4 that changes the
@@ -37,7 +37,11 @@ button's own text to "I'm Clicked!"
 
 let imClickedButton;
 if (imClickedButton) {
-  imClickedButton.addEventListener('click', (evt) => {});
+  imClickedButton.addEventListener('click', (evt) => {
+    document
+        .getElementById("im-clicked-button")
+        .innerHTML = "I'm Clicked!";
+});
 }
 
 /* ====================
