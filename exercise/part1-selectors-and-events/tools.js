145511f5701811b0f41fb4694a851e704e9ba5eb
Alex Nelms <nelmsal@gmail.com>
2022-02-21 14:11:04 -0500

    tools fix

--- exercise/part1-selectors-and-events/tools.js
+++ exercise/part1-selectors-and-events/tools.js
@@ -9,10 +9,11 @@ for more information: https://stackoverflow.com/a/35385518/123776
  * @return {Element}
  */
 function htmlToElement(html) {
-    var template = document.createElement('template');
-    html = html.trim(); // Never return a text node of whitespace as the result
-    template.innerHTML = html;
-    return template.content.firstChild;
+  let template = document.createElement('template');
+  // eslint-disable-next-line no-param-reassign
+  html = html.trim(); // Never return a text node of whitespace as the result
+  template.innerHTML = html;
+  return template.content.firstChild;
 }
 
 /**
@@ -20,7 +21,9 @@ function htmlToElement(html) {
  * @return {NodeList}
  */
 function htmlToElements(html) {
-    var template = document.createElement('template');
-    template.innerHTML = html;
-    return template.content.childNodes;
+  let template = document.createElement('template');
+  template.innerHTML = html;
+  return template.content.childNodes;
 }
+console.log(htmlToElement);
+console.log(htmlToElements);
