6c3b762a6b2d8943a588eea3fe946e1203722b34
capollon <capollon316@gmail.com>
2022-02-28 14:53:07 -0500

    syntax

--- exercise/part1-selectors-and-events/index.js
+++ exercise/part1-selectors-and-events/index.js
@@ -40,9 +40,7 @@ button's own text to "I'm Clicked!"
 let imClickedButton = document.getElementById('im-clicked-button');
 if (imClickedButton) {
   imClickedButton.addEventListener('click', {
-    document
-    .getElementById('im-clicked-button')
-    .innerHTML = 'I'm Clicked!';
+    document.getElementById('im-clicked-button').innerHTML = 'I'm Clicked!';
 });
 }
 
